2c6fb5a235fbbbd5e2dfd74616620aa84408f117
mr-riad <riadgangni01302@gmail.com>
2025-09-04 16:10:48 +0600

    commit the project

---
+++ lib/app/app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class FlutterOnBoardAppTask extends StatefulWidget {
+  const FlutterOnBoardAppTask({super.key});
+
+  @override
+  State<FlutterOnBoardAppTask> createState() => _FlutterOnBoardAppTaskState();
+}
+
+class _FlutterOnBoardAppTaskState extends State<FlutterOnBoardAppTask> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_onboarding_app_task/app/app.dart';
+
+void main(){
+  runApp(FlutterOnBoardAppTask());
+}
