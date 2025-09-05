b4a6615337bd4ca3777e391a8ba9fdbedd0e7e2f
mr-riad <riadgangni01302@gmail.com>
2025-09-05 21:06:11 +0600

    final implement

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 import 'package:device_preview/device_preview.dart';
 import 'package:timezone/data/latest.dart' as tz;
 import 'package:timezone/timezone.dart' as tz;
